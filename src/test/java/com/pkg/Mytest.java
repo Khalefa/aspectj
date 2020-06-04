b811c1d0eae1b56d46b9d6d98d8a4591767fc472
mm <user@MohamedlefasAir.home>
2020-06-03 23:51:49 -0400

    add junit

---
+++ src/test/java/com/pkg/Mytest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertTrue;
+
+public class Mytest {
+
+    @Test
+    public void shouldDoSomeAsserting() {
+        assertTrue(false);
+    }
+
+}
